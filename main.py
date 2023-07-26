b10ef9fac9e1fb993046a44e8eb661c68a2f136e
Simon Valentino <simontvalentino@gmail.com>
2023-07-26 18:46:08 -0400

    Made main.py only have the setup.

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import seaborn as sns
+
+from glob import glob
+
+import librosa
+import librosa.display
+import IPython.display as ipd
+
+from itertools import cycle
+
+sns.set_theme(style="white", palette=None)
+color_pal = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+color_cycle = cycle(plt.rcParams["axes.prop_cycle"].by_key()["color"])
+train_data = glob("./data/train_data/*.wav")
