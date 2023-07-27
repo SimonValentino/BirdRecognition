dffbabff7269b33229e86cf9445b21fa1761838c
Simon Valentino <simontvalentino@gmail.com>
2023-07-26 20:28:22 -0400

    Added data conversion to numbers for ML model to understand

--- main.py
+++ main.py
@@ -14,3 +14,18 @@ from itertools import cycle
 sns.set_theme(style="white", palette=None)
 color_pal = plt.rcParams["axes.prop_cycle"].by_key()["color"]
 color_cycle = cycle(plt.rcParams["axes.prop_cycle"].by_key()["color"])
+
+# get the train audio files
+train = glob("../data/train_data/*.wav")
+
+# loading the audio files with librosa (converting to numbers)
+loaded_train = []
+for audio_file in train:
+    loaded_train.append(librosa.load(audio_file)[0])
+
+# applying fourier transform on data to get Hz over time
+transformed_train = []
+for data in loaded_train:
+    D = librosa.stft(data)
+    S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
+    transformed_train.append(S_db)
