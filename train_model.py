1bc1e5adf23f1e295b0420878019cd4259fe3eb5
Simon Valentino <simontvalentino@gmail.com>
2023-07-29 00:18:48 -0400

    Training the model in train_model.py

---
+++ train_model.py
@@ -0,0 +1,68 @@
+# Imports and setup
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import seaborn as sns
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+from glob import glob
+
+import librosa
+import librosa.display
+import IPython.display as ipd
+
+from itertools import cycle
+
+import os
+from pathlib import Path
+
+sns.set_theme(style="white", palette=None)
+color_pal = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+color_cycle = cycle(plt.rcParams["axes.prop_cycle"].by_key()["color"])
+
+# Getting all the audio files together for training
+audio_files = glob("../data/train_data/*.wav")
+
+# Converting the audio into numbers
+data = {
+    "itemid": [],
+    "data": []
+}
+for file in audio_files:
+    y, sr = librosa.load(file)
+    ps = librosa.feature.melspectrogram(y=y, sr=sr)
+    file_name = Path(file).stem
+    
+    data["itemid"].append(file_name)
+    data["data"].append(ps.flatten())
+  
+# Inital data collection  
+X = pd.DataFrame(data)
+y = pd.read_csv("../data/train_labels.csv")
+
+# Ordering the data so that the audio data lines up with the csv train labels
+merged = X.merge(y, on="itemid", how="inner")
+
+# getting the data after its been ordered
+X = merged["data"].to_frame()
+y = merged["hasbird"]
+
+# Formatting the data for the model
+X_padded = pad_sequences(X["data"], padding='post', dtype='float32')
+X_padded_df = pd.DataFrame(X_padded)
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X_padded_df, y, test_size=0.2)
+
+# Training model and predicting
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+# Accuracy
+acc = accuracy_score(y_test, y_pred)
+print(acc)
