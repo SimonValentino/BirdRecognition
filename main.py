922285bd73b91f4dd746bdd4132c04e2a23bfc04
Simon Valentino <simontvalentino@gmail.com>
2023-07-28 21:54:27 -0400

    Using DecisionTreeClassifier (Not working)

--- main.py
+++ main.py
@@ -11,23 +11,40 @@ import IPython.display as ipd
 
 from itertools import cycle
 
+from pathlib import Path
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
 sns.set_theme(style="white", palette=None)
 color_pal = plt.rcParams["axes.prop_cycle"].by_key()["color"]
 color_cycle = cycle(plt.rcParams["axes.prop_cycle"].by_key()["color"])
 
 # get the train audio files
-train = glob("./data/train_data/*.wav")[:10]
+audio_files = glob("data/train_data/*.wav")
 
-# loading the audio files with librosa (converting to numbers)
-loaded_train = []
-for audio_file in train:
-    loaded_train.append(librosa.load(audio_file)[0])
+# Making audio files usable data
+X_data = [
+    {
+        "itemid": Path(file).stem,
+        "data": librosa.feature.melspectrogram(y=y, sr=sr)
+    }
+    for file in audio_files
+    for y, sr in [librosa.load(file)]
+]
 
-# applying fourier transform on data to get Hz over time
-transformed_train = []
-for data in loaded_train:
-    D = librosa.stft(data)
-    S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
-    transformed_train.append(S_db)
+X = pd.DataFrame(X_data)
 
-print(transformed_train)
+y = pd.read_csv("data/train_labels.csv")
+
+merged = X.merge(y, on="itemid", how="inner")
+
+X = merged["data"]
+y = merged["hasbird"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
